3e1d5a49a07f60fc9e07cc8008710896bf43d99a
Kanta Tamura <kanta03085@gmail.com>
2021-01-30 23:52:44 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#define ROW 3       // 行
+#define COLUMN 3    // 列
+
+int buf[ROW][COLUMN] = {
+        {  1, -2,  3 },
+        {  3, -2,  1 },
+        {  2,  2, -2 },
+};
+
+int buf_a[ROW][1] = {
+        { -1 },
+        { -3 },
+        {  6 },
+};
+
+
+
+int main() {
+
+    return 0;
+}
